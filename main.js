36380fed7c5af542db1de1461981f52347ec3eeb
MIHUIYI689 <MIHUIYI689@pingan.com.cn>
2021-03-24 15:47:50 +0800

    vite项目构建

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: MIHUIYI689
+ * @Date: 2021-03-24 11:39:56
+ * @LastEditors: MIHUIYI689
+ * @LastEditTime: 2021-03-24 15:17:41
+ * @Description: File description
+ * @FilePath: /vue2/main.js
+ */
+import Vue from 'vue/dist/vue';
+import App from ''
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app');
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+// vite.config.js # or vite.config.ts
+
+console.log(path.resolve(__dirname, './dist'))
+
+module.exports = {
+   /**
+   * 在生产中服务时的基本公共路径。
+   * @default '/'
+   */
+  base: '/',
+  /**
+   * 与“根”相关的目录，构建输出将放在其中。如果目录存在，它将在构建之前被删除。
+   * @default 'dist'
+   */
+  outDir: 'target',
+  port: 3000,
+  // 是否自动在浏览器打开
+  open: true,
+  // 是否开启 https
+  https: false,
+  // 服务端渲染
+  ssr: false,
+  // 引入第三方的配置
+  optimizeDeps: {
+  },
+  alias: {
+  },
+  proxy: {
+  }
+}
